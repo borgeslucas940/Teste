a43d1dc55bd12420130511bbecce4dd9985d4bad
Lucas Silva Borges <borgeslucas940@gmail.com>
2020-09-27 10:10:36 -0300

    ex3

--- ex3.c
+++ ex3.c
@@ -14,9 +14,9 @@ END_OF_MAIN()
 
 void init()
 {
-    install_allegro(SYSTEM_NONE, & errno, NULL);
+    install_allegro(SYSTEM_NONE, &errno, NULL);
 
-    BITMAP *bmp= create_bitmap(400, 400);
+    BITMAP *bmp= create_bitmap(100, 100);
     if(!bmp)
     {
         printf("BITMAP nao criado\n");
@@ -25,7 +25,7 @@ void init()
     clear_bitmap(bmp);
 
     set_color_depth(32);
-    circlefill(bmp, 200, 200, 100, makecol(0, 255, 0));
+    circlefill(bmp, 50, 50, 30, makecol(0, 255, 0));
     
     save_bmp("ex3.bmp", bmp, NULL);
     destroy_bitmap(bmp);
